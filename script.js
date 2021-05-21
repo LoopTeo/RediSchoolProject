244a716a93ccb8e53dbd43f192c9b477bb9a8715
Loop Teo <romanstefan24@gmail.com>
2021-05-21 19:50:54 +0200

    homepage almost done

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName("pages")[0];
 toggleButton.addEventListener("click", () => {
 	navbarLinks.classList.toggle("active");
 });
+
+const email = document.getElementById("email");
+
+email.addEventListener("input", function (event) {
+	if (email.validity.typeMismatch) {
+		email.setCustomValidity("I am expecting an e-mail address!");
+	} else {
+		email.setCustomValidity("");
+	}
+});
