6598a3e74fdbc3819634b232982879461ab46343
Loop Teo <romanstefan24@gmail.com>
2021-05-25 12:29:27 +0200

    fixing bugs adding lightbox

--- script.js
+++ script.js
@@ -32,3 +32,29 @@ document.getElementById("submit").addEventListener("click", function () {
 		return false;
 	}
 });
+
+// Lightbox for portofolio
+
+const lightbox = document.createElement("div");
+
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll(".lightbox img");
+images.forEach((image) => {
+	image.addEventListener("click", (e) => {
+		lightbox.classList.add("active");
+
+		const img = document.createElement("img");
+		img.src = image.src;
+		while (lightbox.firstChild) {
+			lightbox.removeChild(lightbox.firstChild);
+		}
+		lightbox.appendChild(img);
+	});
+});
+
+lightbox.addEventListener("click", (e) => {
+	if (e.target !== e.currentTarget) return;
+	lightbox.classList.remove("active");
+});
