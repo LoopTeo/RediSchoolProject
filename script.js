6e64ce9ddfe09069113b589ab097588727a17623
Loop Teo <romanstefan24@gmail.com>
2021-05-29 19:31:35 +0200

    working on contact page

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Hamburger menu
+
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("pages")[0];
 
 toggleButton.addEventListener("click", () => {
 	navbarLinks.classList.toggle("active");
 });
+
+// Subscribe validation
 
 const email = document.getElementById("email");
 
